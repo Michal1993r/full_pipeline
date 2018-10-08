852e4284f60aae3b55af10c0d2a6529b4a8631ab
wowiczek@gmail.com <Chudychudy01>
2018-10-08 22:50:23 +0200

    force https for all requests

--- src/main/java/com/mickl/full_pipeline/config/SsoConfig.java
+++ src/main/java/com/mickl/full_pipeline/config/SsoConfig.java
@@ -2,7 +2,6 @@ package com.mickl.full_pipeline.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +32,10 @@ public class SsoConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/saml*").permitAll()
                 .anyRequest().authenticated()
